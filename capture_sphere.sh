1e08e7bff91c984445cf8a38b453e432c0d1e469
khufkens <koen.hufkens@gmail.com>
2021-02-22 19:06:43 +0100

    update script

--- capture_sphere.sh
+++ capture_sphere.sh
@@ -11,7 +11,6 @@ if [ "$*" == "" ]; then
     echo "No arguments provided"
     echo "use: -u true / false (upload)"
     echo "use: -n true / false (night mode)"
-   	echo "use: -s xxx.xxx.xxx.xxx (ip address / domain name)"
     exit 1
 fi
 
@@ -21,13 +20,13 @@ while getopts ":h?u:n:s:" opt; do
     h|\?)
    		echo "No arguments provided"
     	echo "use: -u true / false (upload)"
-    	echo "use: -s xxx.xxx.xxx.xxx (ip address / domain name)"
+    	echo "use: -n true / false (nightmode)"
     	exit 0
         ;;
     u)  
     	upload=$OPTARG
     	;;
-    s)  
+    n)  
     	server=$OPTARG
     	;;
     :)
@@ -39,10 +38,21 @@ done
 
 # set paths explicitly
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
+TMPDIR="/var/tmp"
 
 # set working directory
 # in which to save the data
-cd /var/tmp/
+cd $TMPDIR
+
+# download github repo if not present
+#  update panorama (overwrite whole repo)
+# removing all previous commits
+if [ ! -d "./theta-z1" ];
+ then
+  echo "Cloning image repo..."
+  # clone repo (change to yours and install ssh access)
+  git clone git@github.com:bluegreen-camera/theta-z1.git
+fi
 
 # make sure to unmount the camera
 # basically in text mode it should not
@@ -81,7 +91,7 @@ fi
 battery=`ptpcam --show-property=0x5001 | grep "to:" | awk '{print $6}'`
 
 # output battery status to file
-echo $battery >> battery_status.txt
+echo $battery > battery_status.txt
 
 if [ "$battery" -lt 20 ];
 then
@@ -106,43 +116,83 @@ for i in $handle;do
 done
 
 
-# loop over 2 exposure settings
-exposures="0 -2000"
+if [[ "$nightmode" == "FALSE" || "$nightmode" == "F" ]] ||
+ [[ "$nightmode" == "f" || "$nightmode" == "false" ]];
+ then
+         
+        # loop over 2 exposure settings
+        exposures="0 -2000"
 	
-ptpcam --set-property=0x500E --val=0x8003 # ISO priority
-ptpcam --set-property=0x5005 --val=0x8002 # set WB to cloudy
-ptpcam --set-property=0x500F --val=100 # set ISO
+        ptpcam --set-property=0x500E --val=0x8003 # ISO priority
+        ptpcam --set-property=0x5005 --val=0x8002 # set WB to cloudy
+        ptpcam --set-property=0x500F --val=100 # set ISO
 	
-for exp in $exposures;do
-	# Change settings of exposure compensation
-	ptpcam --set-property=0x5010 --val=$exp # set compensation
+        for exp in $exposures;do
+	        # Change settings of exposure compensation
+	        ptpcam --set-property=0x5010 --val=$exp # set compensation
 		
-	# snap picture
-	# and wait for it to complete (max 60s)
-	ptpcam -c
-	sleep 60
+	        # snap picture
+	        # and wait for it to complete (max 60s)
+	        ptpcam -c
+	        sleep 60
 	
-	# list last file
-	handle=`ptpcam -L | grep 0x | awk '{print $1}' | sed 's/://g'`
+	        # list last file
+	        handle=`ptpcam -L | grep 0x | awk '{print $1}' | sed 's/://g'`
 	
-	# grab the last file
-	# wait a bit otherwise the next
-	# commands are too fast
-	ptpcam --get-file=$handle
-	sleep 10
+	        # grab the last file
+	        # wait a bit otherwise the next
+	        # commands are too fast
+	        ptpcam --get-file=$handle
+	        sleep 10
 
-	# grab filename of the last downloaded file
-	filename=`ls -t *.JPG | head -1`
+	        # grab filename of the last downloaded file
+	        filename=`ls -t *.JPG | head -1`
 
-	# create filename based upon time / date
-	newfilename=`echo $camera\-exp$exp\_$datetime.jpg`
+	        # create filename based upon time / date
+	        newfilename=`echo $camera\-exp$exp\_$datetime.jpg`
 
-	# rename the last file created
-	mv $filename $newfilename
+	        # rename the last file created
+	        mv $filename $newfilename
 
-	# remove file
-	ptpcam --delete-object=$handle
-done
+	        # remove file
+	        ptpcam --delete-object=$handle
+        done
+
+else
+                
+        ptpcam --set-property=0x500E --val=0x0002 # set normal
+        ptpcam --set-property=0x5005 --val=0x8002 # set WB to cloudy
+        
+        # snap picture
+        # and wait for it to complete (max 60s)
+        ptpcam -c
+        sleep 120
+	
+        # list last file
+        handle=`ptpcam -L | grep 0x | awk '{print $1}' | sed 's/://g'`
+	
+        # grab the last file
+        # wait a bit otherwise the next
+        # commands are too fast
+        ptpcam --get-file=$handle
+        sleep 10
+
+        # grab filename of the last downloaded file
+        filename=`ls -t *.JPG | head -1`
+
+        # create filename based upon time / date
+        newfilename=`echo $camera\-exp0\_$datetime.jpg`
+
+        # rename the last file created
+        mv $filename $newfilename
+
+        # remove file
+        ptpcam --delete-object=$handle
+        sleep 2
+fi
+
+# put camera into sleep mode
+ptpcam ---set-property=0xD80E --val=0x01 # go to sleep
 
 # upload new data to an image server
 if [[ "$upload" == "TRUE" || "$upload" == "T" ]] \
@@ -156,32 +206,26 @@ then
 	        echo "no mask file -mask.png- in current directory"
 	        rm *.jpg
 	        exit 1
-		else		
-		  cp $camera\-exp0\_$datetime.jpg panorama.jpg
-          convert panorama.jpg ~/photosphere/mask.png -compose Multiply -composite panorama.jpg
-        fi
-
-        # download github repo if not present
-        #  update panorama (overwrite whole repo)
-        # removing all previous commits
-        if [ ! -d "./theta-z1" ];
-        then        
-           # clone repo (change to yours and install ssh access)
-           git clone git@github.com:bluegreen-camera/theta-z1.git
+	else		
+          convert $camera\-exp0\_$datetime.jpg ~/photosphere/mask.png\
+           -compose Multiply -composite ./theta-z1/panorama.jpg
         fi
         
         # move data and move into
         # git directory
-        mv panorama.jpg theta-z1/
         cd theta-z1
         
-        # update image   
+        # update image
         git add panorama.jpg
         git commit -am "update"
         git push -f origin main
         
-        # remove files for now
-        rm ../*.jpg
-        
+        # go to base directory
+        cd $TMPDIR
 fi
 
+# remove all files
+rm *.jpg
+
+# done
+exit 0
